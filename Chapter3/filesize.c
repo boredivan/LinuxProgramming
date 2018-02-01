c69579eeb0beaf3d6de2ac65aad535a696a96f00
boredivan <zhouyifan10000@163.com>
2018-02-01 23:33:15 +0800

    today

--- Chapter3/filesize.c
+++ Chapter3/filesize.c
@@ -2,9 +2,11 @@
 #include<stdio.h>
 #include<sys/stat.h>
 
-intmain()
+int main()
 {
 	struct stat infobuf;			/* place to store info */
 	if(stat("/etc/passwd", &infobuf) == -1)	/* get info */
-		perror(
+		perror("/etc/passwd");
+	else
+		printf("The size of /etc/passwd is %d\n", infobuf.st_size);
 }

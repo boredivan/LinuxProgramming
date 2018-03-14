118a2b0e5069d084f0cfca21aa4e26d089b82e30
boredivan <zhouyifan10000@163.com>
2018-03-14 16:01:31 +0800

    Chapter5 part1

---
+++ Chapter5/listchars.c
@@ -0,0 +1,13 @@
+/* listcahrs.c
+ * purpose: list individually all the chars seen on input
+ *  output: char and asscii code, one pair per line
+ *   input: stdin, until the letter Q
+ *   notes: usesful to show that buffering/editing exits
+ */
+#include<stdio.h>
+int main(void)
+{
+	int c, n = 0;
+	while((c = getchar()) != 'Q')
+		printf("char %3d is %c code %d\n", n++, c, c);
+}
